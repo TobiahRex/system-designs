6fb2fdab1b9c510396016d534237b399ceb56769
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-09 16:55:10 -0800

    building out "sliding window" time window.

--- api-rate-limiter/fixed-window-algo/sliding-window.js
+++ api-rate-limiter/fixed-window-algo/sliding-window.js
@@ -1,4 +1,4 @@
-let memo = {}, GLOBAL_COUNT_LIMIT = 3;
+let memo = {}, GLOBAL_COUNT_LIMIT = 2;
 
 let intervalId = setInterval(() => {
   let users = ['bob', 'john', 'padro', 'pablo', 'flaco'],
@@ -19,14 +19,12 @@ function main({ user_id, time_stamp }){
     // but a second has past since their last request, so
     // 1) reset their count,
     // 2) and save the time.
-    let savedReqs = memo[user_id]
-      .filter((time) => (time_stamp - time < 1000) ? false : true)
-      .length
-
-    if (savedReqs < GLOBAL_COUNT_LIMIT) {
-      memo[user_id].push(time_stamp);
-    }
-
+    if (memo[user_id].time - time_stamp >= 1000) {
+      memo[user_id].time = time_stamp;
+      memo[user_id].count = 1;
+      // the user's ID is already and a second has not yet passed, so verify their count is below tolerance...
+      // if so, allow the request to pass through.
+      // if not, reject the request.
     } else {
       if (memo[user_id].count >= GLOBAL_COUNT_LIMIT) {
         return ('429 | Rate limit exceeded. Please try again later.');
