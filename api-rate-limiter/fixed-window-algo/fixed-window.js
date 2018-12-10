60e6e5787f0a0dfe654ebebee0c494379f199190
Tobiah Rex <tobiah.rex@gmail.com>
2018-12-09 16:56:57 -0800

    finsihed building fixed-window.

--- api-rate-limiter/fixed-window-algo/fixed-window.js
+++ api-rate-limiter/fixed-window-algo/fixed-window.js
@@ -2,7 +2,7 @@ let memo = {}, GLOBAL_COUNT_LIMIT = 3;
 
 let intervalId = setInterval(() => {
   let users = ['bob', 'john', 'padro', 'pablo', 'flaco'],
-    randomUser = users[Math.floor(Math.random() * 4)];
+  randomUser = users[Math.floor(Math.random() * 4)];
 
   console.log(`   Init ${randomUser} request...`);
   console.log(main({ user_id: randomUser, time_stamp: Date.now() }));
@@ -20,25 +20,16 @@ function main({ user_id, time_stamp }){
     // 1) reset their count,
     // 2) and save the time.
     let savedReqs = memo[user_id]
-      .filter((time) => (time_stamp - time < 1000) ? false : true)
-      .length
+    .filter((time) => (time_stamp - time < 1000) ? false : true)
+    .length
 
     if (savedReqs < GLOBAL_COUNT_LIMIT) {
       memo[user_id].push(time_stamp);
-    }
-
     } else {
-      if (memo[user_id].count >= GLOBAL_COUNT_LIMIT) {
-        return ('429 | Rate limit exceeded. Please try again later.');
-      } else {
-        memo[user_id].count += 1;
-      }
+      return new Error('429: Request limit exceeded.');
     }
   } else {
-    memo[user_id] = {
-      count: 1,
-      time: time_stamp,
-    }
+    memo[user_id] = [time_stamp];
   }
   return `${user_id} request ACCEPTED`;
 }
